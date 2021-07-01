4e88ca3be8759bb2f3a4a9df153f4fd01cff7b9f
AngelDTV <angelj.dtv@gmail.com>
2021-06-30 19:12:28 -0500

    new

--- database/migrations/1625086038635_users.ts
+++ database/migrations/1625086038635_users.ts
@@ -6,10 +6,14 @@ export default class UsersSchema extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('name', 200).notNullable()
+      table.string('lastname', 200).nullable()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
+      table.string('profile_photo').notNullable()
+      table.integer('phone_number', 13).nullable()
+      table.date('birthday').nullable()
       table.string('remember_me_token').nullable()
-
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
